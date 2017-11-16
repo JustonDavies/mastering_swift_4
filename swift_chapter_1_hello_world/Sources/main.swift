542fb659b6492e714c91a38b60f5f024dbcebae0
Juston C. Davies <Juston.Davies@gmail.com>
2017-11-16 10:56:57 -0700

    Chapter 1 - Complete Chapter 2 - Started

---
+++ swift_chapter_1_hello_world/Sources/main.swift
@@ -0,0 +1,23 @@
+//-- Exercise 0 ----------
+print("Hello World!")
+
+//-- Exercise 1 ----------
+var name = "Jon"
+var language = "Swift"
+
+var message1 = ", Welcome to the wonderful world of "
+var message2 = "\(name), Welcome to the wonderful world of \(language)!"
+
+print(message2)
+print(name, message1, language, "!", separator: "")
+
+//-- Exercise 2 ----------
+var name1 = "Jon"
+var name2 = "Kim"
+var name3 = "Kailey"
+var name4 = "Kara"
+
+var line = "";
+
+print(name1, name2, name3, name4, separator:", ", terminator:"", to:&line)
+print(line)
